f60b649d8bf1026a5bae4a5bed6619e2c0b815a9
Saeeda <saeedabb@gmail.com>
2024-09-15 14:18:03 +0500

    Test 12

--- PopupWindow/index.js
+++ PopupWindow/index.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/about', (req, res) => {
+  res.sendFile('Test');
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
